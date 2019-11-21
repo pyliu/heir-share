1266459605f4c4f3498ae52f9d1a30e6bc8b3037
pyliu <3634168+pyliu@users.noreply.github.com>
2019-11-22 00:08:44 +0800

    breadcrumb back

--- src/renderer/components/HeirShare.vue
+++ src/renderer/components/HeirShare.vue
@@ -1,104 +1,168 @@
 <template>
-  <div id="wrapper" class="m-2">
-    <div class="mb-2">
-      <span class="text-danger font-weight-bold">＊</span>
-      被繼承人持分：
-      <input type="number" min="1" style="width:4rem" v-model="heir_denominator" @change="filter" />
-      分之 1
-    </div>
-    <fieldset class="border p-2" v-show="wizard.s0.seen">
+    <b-container id="wrapper" fluid :class="['mt-2']">
+        <nav aria-label="breadcrumb" class="shadow">
+            <!-- <b-breadcrumb :items="breadcrumb" :class="['bg-dark', 'text-white']"></b-breadcrumb> -->
+            <ol class="breadcrumb bg-dark text-white">
+                <li
+                v-for="(item, index) in breadcrumb"
+                v-bind:class="{active : index === (breadcrumb.length - 1)}"
+                >
+                <small>{{index == 0 ? item.legend : item.legend + "　/　"}}</small>
+                </li>
+            </ol>
+        </nav>
+        <div class="row mb-2">
+            <div class="col">
+                <span class="text-danger font-weight-bold">＊</span>
+                被繼承人持分：
+                <input
+                    type="number"
+                    min="1"
+                    style="width:4rem"
+                    v-model="heir_denominator"
+                    @change="filter"
+                />
+                分之 1
+            </div>
+            <div class="col-3">
+                <b-btn-group size="sm">
+                    <b-button @click="prev">&lt;</b-button>
+                    <b-button>|</b-button>
+                    <b-button @click="next">&gt;</b-button>
+                </b-btn-group>
+            </div>
+        </div>
+        <fieldset class="border p-2" v-show="wizard.s0.seen">
         <legend class="w-auto">{{wizard.s0.legend}}</legend>
         <div class="row text-center">
-          <label class="col-4">
+            <label class="col-4">
             <input type="radio" v-model.number="wizard.s0.value" value="-1" @change="s0ValueSelected" /> 光復前【民國34年10月24日以前】
-          </label>
-          <label class="col-4">
+            </label>
+            <label class="col-4">
             <input type="radio" v-model.number="wizard.s0.value" value="0" @change="s0ValueSelected" /> 光復後【民國74年6月4日以前】
-          </label>
-          <label class="col-4">
+            </label>
+            <label class="col-4">
             <input type="radio" v-model.number="wizard.s0.value" value="1" @change="s0ValueSelected" /> 光復後【民國74年6月5日以後】
-          </label>
+            </label>
         </div>
-    </fieldset>
+        </fieldset>
 
-    <!-- step 1 光復前 -->
-    <fieldset class="border p-2" v-show="wizard.s1.seen">
+        <!-- step 1 光復前 -->
+        <fieldset class="border p-2" v-show="wizard.s1.seen">
         <legend class="w-auto">{{wizard.s1.legend}}</legend>
         <div class="row text-center">
-          <label class="col-6">
+            <label class="col-6">
             <input type="radio" v-model="wizard.s1.value" value="public" @change="s1ValueSelected" /> 家產
-          </label>
-          <label class="col-6">
+            </label>
+            <label class="col-6">
             <input type="radio" v-model="wizard.s1.value" value="private" @change="s1ValueSelected" /> 私產
-          </label>
+            </label>
         </div>
         <div class="border-top border-primary pt-2" v-show="seen_s1_public">
-          <ol class="d-block">
+            <ol class="d-block">
             <li>
-              法定推定財產繼承人係
-              <strong class="text-primary">男子</strong>直系卑親屬，以親等近者為優先。親等相同之
-              <strong class="text-primary">男子</strong>有數人時，共同均分繼承。
+                法定推定財產繼承人係
+                <strong class="text-primary">男子</strong>直系卑親屬，以親等近者為優先。親等相同之
+                <strong class="text-primary">男子</strong>有數人時，共同均分繼承。
             </li>
             <li>無法定之推定戶主繼承人時，指定及選定之財產繼承人繼承。</li>
-          </ol>人數： <input type="number" min="0" class="num-counter" v-model="wizard.s1.public.count" @change="filter" />
-          <span v-show="seen_s1_pub_msg">
+            </ol>人數：
+            <input
+            type="number"
+            min="0"
+            class="num-counter"
+            v-model="wizard.s1.public.count"
+            @change="filter"
+            />
+            <span v-show="seen_s1_pub_msg">
             每人之應繼份為
             <span class="text-primary">{{wizard.s1.public.count * heir_denominator}} 分之 1</span>。
-          </span>
+            </span>
         </div>
         <div class="border-top border-primary pt-2" v-show="seen_s1_private">
-          <h6>* 僅有法定繼承人，順序如下：</h6>
-          <ol class="d-block">
+            <h6>* 僅有法定繼承人，順序如下：</h6>
+            <ol class="d-block">
             <li v-show="seen_s1_private_1">
-              人數：
-              <input type="number" min="0" class="num-counter" v-model="wizard.s1.private.child" @change="filter" />
-              【直系卑親屬，以親等近者為優先。親等相同之男子有數人時，共同均分之。】
-              <br />
-              <span v-show="seen_s1_private_1_msg">
+                人數：
+                <input
+                type="number"
+                min="0"
+                class="num-counter"
+                v-model="wizard.s1.private.child"
+                @change="filter"
+                />
+                【直系卑親屬，以親等近者為優先。親等相同之男子有數人時，共同均分之。】
+                <br />
+                <span v-show="seen_s1_private_1_msg">
                 直系卑親屬每人之應繼份為
-                <span class="text-primary">{{wizard.s1.private.child * heir_denominator}} 分之 1</span>。
-              </span>
+                <span
+                    class="text-primary"
+                >{{wizard.s1.private.child * heir_denominator}} 分之 1</span>。
+                </span>
             </li>
             <li v-show="seen_s1_private_2">
-              人數：
-              <input type="number" min="0" max="1" class="num-counter" v-model="wizard.s1.private.spouse" @change="filter" />
-              【配偶】
-              <br />
-              <span v-show="seen_s1_private_2_msg">
+                人數：
+                <input
+                type="number"
+                min="0"
+                max="1"
+                class="num-counter"
+                v-model="wizard.s1.private.spouse"
+                @change="filter"
+                />
+                【配偶】
+                <br />
+                <span v-show="seen_s1_private_2_msg">
                 配偶應繼份為
                 <span class="text-primary">{{wizard.s1.private.spouse * heir_denominator}} 分之 1</span>。
-              </span>
+                </span>
             </li>
             <li v-show="seen_s1_private_3">
-              人數：
-              <input type="number" min="0" class="num-counter" v-model="wizard.s1.private.parent" @change="filter" />
-              【直系尊親屬，親等不同以親等近者為先，同一親等有2人以上，共同均分之。】
-              <br />
-              <span v-show="seen_s1_private_3_msg">
+                人數：
+                <input
+                type="number"
+                min="0"
+                class="num-counter"
+                v-model="wizard.s1.private.parent"
+                @change="filter"
+                />
+                【直系尊親屬，親等不同以親等近者為先，同一親等有2人以上，共同均分之。】
+                <br />
+                <span v-show="seen_s1_private_3_msg">
                 直系尊親屬每人之應繼份為
-                <span class="text-primary">{{wizard.s1.private.parent * heir_denominator}} 分之 1</span>。
-              </span>
+                <span
+                    class="text-primary"
+                >{{wizard.s1.private.parent * heir_denominator}} 分之 1</span>。
+                </span>
             </li>
             <li v-show="seen_s1_private_4">
-              人數：
-              <input type="number" min="0" max="1" class="num-counter" v-model="wizard.s1.private.household" @change="filter" />
-              【戶主】
-              <br />
-              <span v-show="seen_s1_private_4_msg">
+                人數：
+                <input
+                type="number"
+                min="0"
+                max="1"
+                class="num-counter"
+                v-model="wizard.s1.private.household"
+                @change="filter"
+                />
+                【戶主】
+                <br />
+                <span v-show="seen_s1_private_4_msg">
                 戶主應繼份為
-                <span class="text-primary">{{wizard.s1.private.household * heir_denominator}} 分之 1</span>。
-              </span>
+                <span
+                    class="text-primary"
+                >{{wizard.s1.private.household * heir_denominator}} 分之 1</span>。
+                </span>
             </li>
-          </ol>
+            </ol>
         </div>
-    </fieldset>
+        </fieldset>
 
-    <!-- step 2 光復後 -->
-    <fieldset class="border p-2" v-show="wizard.s2.seen">
-        <legend class="w-auto">{{wizard.s2.legend}}</legend>
-        TODO ...
-    </fieldset>
-  </div>
+        <!-- step 2 光復後 -->
+        <fieldset class="border p-2" v-show="wizard.s2.seen">
+        <legend class="w-auto">{{wizard.s2.legend}}</legend>TODO ...
+        </fieldset>
+    </b-container>
 </template>
 
 <script>
@@ -270,7 +334,7 @@ export default {
       return this.wizard.s1.private.household > 0;
     }
   },
-  components: { },
+  components: {},
   mounted: function() {
     // like jQuery ready
     this.now_step = this.wizard.s0;
@@ -284,10 +348,10 @@ export default {
 
 <style>
 #wrapper {
-	font-family: "微軟正黑體", "Microsoft JhenHei", sans-serif;
-  font-size: .85rem;
+  font-family: "微軟正黑體", "Microsoft JhenHei", sans-serif;
+  font-size: 0.85rem;
 }
 .num-counter {
-   width: 2.2rem;
+  width: 2.2rem;
 }
 </style>
